3b8480a09dc2ec9a916983d588d276689110ff40
Randika Wanninayaka <randika@arimaclanka.com>
2025-05-10 14:53:31 +0530

    Initial Commit

---
+++ LankaBusRideSwiftUI/LankaBusRideSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  LankaBusRideSwiftUIApp.swift
+//  LankaBusRideSwiftUI
+//
+//  Created by Arimac on 2025-05-10.
+//
+
+import SwiftUI
+
+@main
+struct LankaBusRideSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
