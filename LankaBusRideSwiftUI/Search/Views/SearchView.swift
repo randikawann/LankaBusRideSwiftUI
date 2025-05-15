ad5f72bc9824bebcc52202a0b14d495e0c63a4c6
Randika Wanninayaka <randika@arimaclanka.com>
2025-05-15 12:20:38 +0530

    added navigation from search card to detail view

--- LankaBusRideSwiftUI/Search/Views/SearchView.swift
+++ LankaBusRideSwiftUI/Search/Views/SearchView.swift
@@ -11,54 +11,58 @@ struct SearchView: View {
     @StateObject private var viewModel = SearchViewModel()
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Color("LightGreen"))
-                .frame(height: 100)
-                .overlay(
-                    VStack{
-                        Text("Your location")
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.horizontal, 16)
-                            .foregroundColor(Color("PrimaryGreen"))
-                        TextField("Enter route number", text: $viewModel.routeNumber)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding(.horizontal, 16)
-                            .disabled(true)
-                    }
-                )
-                .padding(.horizontal, 16)
-                .shadow(color: .gray.opacity(0.4), radius: 6, x: 0, y: 4)
-            Text("Recent Routes")
-                .font(.headline)
-                .padding(.top)
+        NavigationView {
             VStack {
-                recentRoutesList
-                    .listStyle(PlainListStyle())
-            }.frame(height: 150.0)
-            
-            ScrollView {
-                if viewModel.routeNumber.isEmpty {
-                    VStack(alignment: .leading) {
-                        Text("No searched values. Please select a Recent route")
-                            .foregroundColor(.gray)
-                            .padding()
-                    }
-                } else {
-                    VStack(spacing: 12) {
-                        ForEach(viewModel.filteredBusRoutes, id: \.self) { route in
-                            AvailableBusRow(bus: route)
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color("LightGreen"))
+                    .frame(height: 100)
+                    .overlay(
+                        VStack{
+                            Text("Your location")
+                                .frame(maxWidth: .infinity, alignment: .leading)
                                 .padding(.horizontal, 16)
+                                .foregroundColor(Color("PrimaryGreen"))
+                            TextField("Enter route number", text: $viewModel.routeNumber)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding(.horizontal, 16)
+                                .disabled(true)
                         }
+                    )
+                    .padding(.horizontal, 16)
+                    .shadow(color: .gray.opacity(0.4), radius: 6, x: 0, y: 4)
+                Text("Recent Routes")
+                    .font(.headline)
+                    .padding(.top)
+                VStack {
+                    recentRoutesList
+                        .listStyle(PlainListStyle())
+                }.frame(height: 150.0)
+                
+                ScrollView {
+                    if viewModel.routeNumber.isEmpty {
+                        VStack(alignment: .leading) {
+                            Text("No searched values. Please select a Recent route")
+                                .foregroundColor(.gray)
+                                .padding()
+                        }
+                    } else {
+                        VStack(spacing: 12) {
+                            ForEach(viewModel.filteredBusRoutes, id: \.self) { route in
+                                NavigationLink(destination: DetailView(busID: route.id)) {
+                                    AvailableBusRow(bus: route)
+                                        .padding(.horizontal, 16)
+                                }
+                            }
+                        }
+                        .padding(.vertical, 8)
                     }
-                    .padding(.vertical, 8)
                 }
+                
+                Divider().padding(.vertical)
             }
-            
-            Divider().padding(.vertical)
-        }
-        .onAppear {
-            viewModel.loadTopRoutes()
+            .onAppear {
+                viewModel.loadTopRoutes()
+            }
         }
     }
     
